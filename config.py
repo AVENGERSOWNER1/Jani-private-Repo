26e06abf48f971d7dfeb16fca30ec48331e45197
AVENGERSOWNER1 <akgameing452@gmail.com>
2025-09-09 21:41:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/JaniRPLover/Jani-private-Repo",
+    "https://github.com/AVENGERSOWNER1/Jani-private-Repo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "jani")
 GIT_TOKEN = getenv(
