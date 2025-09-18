d4fea2b5a83f6f20e8351e534a5af228db673c4c
AVENGERSOWNER1 <akgameing452@gmail.com>
2025-09-18 23:23:34 +0545

    Update config.py

--- config.py
+++ config.py
@@ -81,10 +81,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/0rl8s0.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/bqgiu5.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/0rl8s0.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/bqgiu5.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
